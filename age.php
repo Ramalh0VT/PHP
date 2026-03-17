286cb35062851b5b5dd87ab1e8975fd0ce91a648
GUSTAVO RAMALHO DA SILVA <25254294@educ123.sp.senai.br>
2026-03-17 16:56:58 -0300

    made another php code

---
+++ age.php
@@ -0,0 +1,7 @@
+<?php
+
+$grade1 = 7
+$grade2 = 6 
+$grade3 = 5
+
+$sum
---
+++ first.php
@@ -0,0 +1,15 @@
+<?php
+echo "Olá Mundo!!";
+
+$num1 = 149;
+$num2 = 49;
+$soma =$num1 + $num2;
+
+print("<br>");
+
+print ("A soma de $num1 e $num2 é igual a $soma");
+
+if ($age >= 18) {
+    echo "Your age"
+}
+?>
