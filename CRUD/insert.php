5dd38279b858cb2a3623c3f2ebfeccddd6eccecf
Ramalh0VT <gustavoramalhodasilva777@gmail.com>
2026-05-05 15:12:36 -0300

    Started making the crud more advanced

--- CRUD/insert.php
+++ CRUD/insert.php
@@ -2,15 +2,38 @@
 require_once 'crud.php';
 
 $novoLivro = [
-	'titulo' => 'PHP for dummies',
-	'isbn' => '9781118008',
-	'autor' => 'John Doe',
-	'preco' => 299.99,
-	'situacao' => 'Disponivel',
-	'Categoria' => 'Informática'
+	'titulo' => $_POST['titulo'],
+	'isbn' => $_POST['isbn'],
+	'autor' => $_POST['Autor'],
+	'preco' => $_POST['preco'],
+	'situacao' => $_POST['situacao'],
+	'categoria' => $_POST['categoria']
+	'capa' => '' 
 ];
 
-$idLivroNovo = create($pdo, 'livros', $novoLivro);
-echo 'novo Livro inserido com ID: '.$idLivroNovo.'';
+$idLivroNovo = create($pdo, 'livros', $novoLivro
 
+$tipos_permitidos = ['image/jpeg', 'image/png', 'image/gif'];
+
+
+if (!in_array($_FILES['arquivo']['type'], $tipos_permitidos)) {
+	echo "Tipo de arquivo não permitido. Por favor, envie uma jpeg, png ou gif.";
+	die();
+}
+
+$tamanho_max = 1 * 1024 * 1024;
+if ($_FILES['arquivo']['size'] > $tamanho_max) {
+
+	echo "O arquivo é muito grande! O tamanho máximo permitido é de 1MB";
+	die();
+}
+
+$extensao = pathinfo($_FILES['arquivos']['name'],PATHINFO_EXTENSION);
+
+
+$novonome = "capa_".uniqid().".".$extensao;
+
+$dir = "uploads/";
+
+$caminho = $dir
 ?>
