b0f795f1f2186f8ec3446fe2092e7ed0c7b5aa50
Ramalh0VT <gustavoramalhodasilva777@gmail.com>
2026-05-05 14:56:54 -0300

    finished the form

--- CRUD/form.php
+++ CRUD/form.php
@@ -1,5 +1,3 @@
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,25 +7,25 @@
 </head>
 <body>
 	<main>
-		<form>
-			<label for="titulo">Titulo</label>
-				<input type="text" id="titulo" name="titulo" placeholder="titulo" required maxlength="255">
-			<label for="isbn">isbn</label>
-				<input type="text" id="isbn" name="isbn" placeholder="isbn" maxlength="20">
-			<label for="autor">Autor</label>
-				<input type="text" id="autor" name="autor" placeholder="autor"maxlength="255">
-			<label for="preco">Preço</label>
-				<input type="number" maxlength="12" step="0.01" id="preco" name="preco" placeholder="Preço">
-			<label for="situacao">Situação</label>
+		<form action="insert.php" method="POST" enctype="multipart/form-data">
+			<label for="titulo">Titulo</label><br>
+				<input type="text" id="titulo" name="titulo" placeholder="titulo (Obrigatório)" required maxlength="255"><br>
+			<label for="isbn">isbn</label><br>
+				<input type="text" id="isbn" name="isbn" placeholder="isbn" maxlength="20"><br>
+			<label for="autor">Autor</label><br>
+				<input type="text" id="autor" name="autor" placeholder="autor"maxlength="255"><br>
+			<label for="preco">Preço</label><br>
+				<input type="number" maxlength="12" step="0.01" id="preco" name="preco" placeholder="Preço"><br>
+			<label for="situacao">Situação</label><br>
 				<select>
 					<option>Disponível</option>
 					<option>Indisponível</option>
-				</select>
-			<label for="categoria">Categoria</label>
-				<input type="text" maxlength="100" id="categoria" name="categoria" placeholder="categoria">
-			<label for="capa">Capa</label>
-				<input type="file" maxlength="1000" id="capa" name="capa">
-			<button type="submit">ENVIAR</button>
+				</select><br>
+			<label for="categoria">Categoria</label><br>
+				<input type="text" maxlength="100" id="categoria" name="categoria" placeholder="categoria"><br>
+			<label for="capa">Capa</label><br>
+				<input type="file" maxlength="1000" id="capa" accept="image/*" name="capa"><br>
+			<button type="submit">ENVIAR</button><br>
 		</form>
 	<main>
 </body>
